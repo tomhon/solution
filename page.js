2a24bc7c72bf4c4ccc67bb23cda85d48758676ae
tomhon <tomhon@hotmail.com>
2015-07-24 22:47:30 -0700

    3rd commit with facebook authentication

--- page.js
+++ page.js
@@ -58,5 +58,35 @@ $(function() {
     });
 
     // On initial load, start by fetching the current data
-    refreshTodoItems();
+    //refreshTodoItems();
+    function refreshAuthDisplay() {
+        var isLoggedIn = client.currentUser !== null;
+        $("#logged-in").toggle(isLoggedIn);
+        $("#logged-out").toggle(!isLoggedIn);
+
+        if (isLoggedIn) {
+            $("#login-name").text(client.currentUser.userId);
+            refreshTodoItems();
+        }
+    }
+
+    function logIn() {
+        client.login("facebook").then(refreshAuthDisplay, function (error) {
+            alert(error);
+        });
+    }
+
+    function logOut() {
+        client.logout();
+        refreshAuthDisplay();
+        $('#summary').html('<strong>You must login to access data.</strong>');
+    }
+
+    // On page init, fetch the data and set up event handlers
+    $(function () {
+        refreshAuthDisplay();
+        $('#summary').html('<strong>You must login to access data.</strong>');
+        $("#logged-out button").click(logIn);
+        $("#logged-in button").click(logOut);
+    });
 });
